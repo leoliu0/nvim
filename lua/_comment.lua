74b6596e0d38aefae2715e95ef82d282f9d8110b
Leo Liu <auleo.liu@gmail.com>
2022-01-03 16:39:46 +1100

    add tex snip

--- lua/_comment.lua
+++ lua/_comment.lua
@@ -5,27 +5,38 @@ end
 
 comment.setup({
 	toggler = {
-		line = "<C-_>",
-		block = "gbc",
+		line = "gl",
+		-- block = "gbc",
 	},
-
 	opleader = {
-		line = "<C-_>",
-		block = "gb",
+		line = "gll",
+		-- 	block = "gb",
 	},
-	pre_hook = function(ctx)
-		local U = require("Comment.utils")
-
-		local location = nil
-		if ctx.ctype == U.ctype.block then
-			location = require("ts_context_commentstring.utils").get_cursor_location()
-		elseif ctx.cmotion == U.cmotion.v or ctx.cmotion == U.cmotion.V then
-			location = require("ts_context_commentstring.utils").get_visual_start_location()
-		end
-
-		return require("ts_context_commentstring.internal").calculate_commentstring({
-			key = ctx.ctype == U.ctype.line and "__default" or "__multiline",
-			location = location,
-		})
-	end,
+	mappings = {
+		---Operator-pending mapping
+		---Includes `gcc`, `gbc`, `gc[count]{motion}` and `gb[count]{motion}`
+		---NOTE: These mappings can be changed individually by `opleader` and `toggler` config
+		basic = true,
+		---Extra mapping
+		---Includes `gco`, `gcO`, `gcA`
+		extra = false,
+		---Extended mapping
+		---Includes `g>`, `g<`, `g>[count]{motion}` and `g<[count]{motion}`
+		extended = false,
+	},
+	-- pre_hook = function(ctx)
+	-- 	local U = require("Comment.utils")
+	--
+	-- 	local location = nil
+	-- 	if ctx.ctype == U.ctype.block then
+	-- 		location = require("ts_context_commentstring.utils").get_cursor_location()
+	-- 	elseif ctx.cmotion == U.cmotion.v or ctx.cmotion == U.cmotion.V then
+	-- 		location = require("ts_context_commentstring.utils").get_visual_start_location()
+	-- 	end
+	--
+	-- 	return require("ts_context_commentstring.internal").calculate_commentstring({
+	-- 		key = ctx.ctype == U.ctype.line and "__default" or "__multiline",
+	-- 		location = location,
+	-- 	})
+	-- end,
 })
